9350bf853e3248159c1e3ef569f467850f35cb4b
Masatake Aoki <aoki@rt-net.jp>
2023-10-29 19:24:12 +0900

    add step11,mod directory step10

---
+++ uROS_STEP10_tfMsg/interrupt.ino
@@ -0,0 +1,60 @@
+// Copyright 2023 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+void controlInterrupt(void)
+{
+  // 直進速度と回転速度から、左右のモータ速度を求める
+  double speed_r = g_speed + g_omega * TREAD_WIDTH / 2.0;
+  double speed_l = g_speed - g_omega * TREAD_WIDTH / 2.0;
+
+  // 左右両方のモータ速度がMIN_SPEED以下の場合は走行を停止する
+  if (fabs(speed_r) < MIN_SPEED && fabs(speed_l) < MIN_SPEED) {
+    g_motor_move = 0;
+    return;
+  }
+
+  g_motor_move = 1;
+  // モータ速度をMIN_SPEED以上に制限する
+  if (fabs(speed_r) < MIN_SPEED) {
+    speed_r = (speed_r > 0.0) ? MIN_SPEED : -1.0 * MIN_SPEED;
+  }
+  if (fabs(speed_l) < MIN_SPEED) {
+    speed_l = (speed_l > 0.0) ? MIN_SPEED : -1.0 * MIN_SPEED;
+  }
+
+  // 制限されたモータ速度から、直進速度と回転速度を求める
+  const double forward_speed = (speed_r + speed_l) / 2.0;
+  const double omega = (speed_r - speed_l) / TREAD_WIDTH;
+
+  const double UPDATE_INTERVAL = 0.001;
+  g_odom_x += forward_speed * UPDATE_INTERVAL * cos(g_odom_theta) * UPDATE_INTERVAL;
+  g_odom_y += forward_speed * UPDATE_INTERVAL * sin(g_odom_theta) * UPDATE_INTERVAL;
+  g_odom_theta += omega * UPDATE_INTERVAL;
+  g_position_r += speed_r * UPDATE_INTERVAL / (TIRE_DIAMETER * PI) * 2 * PI;
+  g_position_l -= speed_l * UPDATE_INTERVAL / (TIRE_DIAMETER * PI) * 2 * PI;
+
+  if (speed_r > 0) {
+    digitalWrite(CW_R, HIGH);
+  } else {
+    digitalWrite(CW_R, LOW);
+  }
+  g_step_hz_r = abs((signed short)(speed_r / PULSE));
+
+  if (speed_l > 0) {
+    digitalWrite(CW_L, LOW);
+  } else {
+    digitalWrite(CW_L, HIGH);
+  }
+  g_step_hz_l = abs((signed short)(speed_l / PULSE));
+}
---
+++ uROS_STEP11_SensorMsg/uROS_STEP11_SensorMsg.ino
@@ -0,0 +1,181 @@
+// Copyright 2023 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// clang-format off
+// メッセージヘッダーファイルを見つけるため、micro_ros_arduino.hを先にインクルードすること
+#include <micro_ros_arduino.h>
+#include <pico_msgs/msg/light_sensor.h>
+#include <rcl/error_handling.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <stdio.h>
+#include <std_msgs/msg/int16.h>
+// clang-format off
+
+pico_msgs__msg__LightSensor g_sensor_msg;
+std_msgs__msg__Int16 g_bat_msg;
+
+rcl_publisher_t g_publisher_sensor, g_publisher_battery;
+rclc_support_t g_support;
+rcl_allocator_t g_allocator;
+rcl_node_t g_node;
+
+#define LED0 42
+#define LED1 41
+#define LED2 15
+#define LED3 16
+
+#define SLED_F 2
+#define SLED_S 1
+
+#define AD4 5
+#define AD3 4
+#define AD2 7
+#define AD1 6
+#define AD0 8
+
+hw_timer_t * g_timer1 = NULL;
+
+portMUX_TYPE g_timer_mux = portMUX_INITIALIZER_UNLOCKED;
+
+volatile short g_sensor_value_fr;
+volatile short g_sensor_value_fl;
+volatile short g_sensor_value_r;
+volatile short g_sensor_value_l;
+volatile short g_battery_value;
+
+#define RCCHECK(fn)                \
+  {                                \
+    rcl_ret_t temp_rc = fn;        \
+    if ((temp_rc != RCL_RET_OK)) { \
+      errorLoop();                \
+    }                              \
+  }
+#define RCSOFTCHECK(fn)            \
+  {                                \
+    rcl_ret_t temp_rc = fn;        \
+    if ((temp_rc != RCL_RET_OK)) { \
+    }                              \
+  }
+
+void errorLoop()
+{
+  while (1) {
+    digitalWrite(LED0, !digitalRead(LED0));
+    delay(100);
+  }
+}
+
+void IRAM_ATTR onTimer1(void)
+{
+  static char cnt = 0;
+  portENTER_CRITICAL_ISR(&g_timer_mux);
+  short temp_r,temp_l;
+  switch (cnt) {
+    case 0:
+      temp_r = analogRead(AD4);
+      temp_l = analogRead(AD1);
+      digitalWrite(SLED_F, HIGH);  //LED点灯
+      for (int i = 0; i < 10; i++) {
+        asm("nop \n");
+      }
+      g_sensor_value_fr = analogRead(AD4) - temp_r;
+      g_sensor_value_fl = analogRead(AD1) - temp_l;
+      digitalWrite(SLED_F, LOW);  //LED消灯
+      break;
+    case 1:
+      temp_r = analogRead(AD3);
+      temp_l = analogRead(AD2);
+      digitalWrite(SLED_S, HIGH);  //LED点灯
+      for (int i = 0; i < 10; i++) {
+        asm("nop \n");
+      }
+      g_sensor_value_r = analogRead(AD3) - temp_r;
+      g_sensor_value_l = analogRead(AD2) - temp_l;
+      digitalWrite(SLED_S, LOW);  //LED消灯
+      g_battery_value = (double)analogReadMilliVolts(AD0) / 10.0 * (10.0 + 51.0);
+      break;
+    default:
+      Serial.printf("error\n\r");
+      break;
+  }
+  cnt++;
+  if (cnt == 2) cnt = 0;
+  portEXIT_CRITICAL_ISR(&g_timer_mux);
+}
+
+void publisherTask(void * pvParameters)
+{
+  while (1) {
+    g_sensor_msg.forward_r = g_sensor_value_fr;
+    g_sensor_msg.forward_l = g_sensor_value_fl;
+    g_sensor_msg.right = g_sensor_value_r;
+    g_sensor_msg.left = g_sensor_value_l;
+    g_bat_msg.data = g_battery_value;
+    RCSOFTCHECK(rcl_publish(&g_publisher_sensor, &g_sensor_msg, NULL));
+    RCSOFTCHECK(rcl_publish(&g_publisher_battery, &g_bat_msg, NULL));
+    delay(10);
+  }
+}
+
+void setup()
+{
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  digitalWrite(LED1, HIGH);
+//  set_microros_wifi_transports("使用するWiFiのAP名", "Wi-Fiのパスワード", "PCのIPアドレス", 8888);
+  set_microros_wifi_transports("SPWH_L12_dd755e", "281b56e7f59ad", "192.168.100.51", 8888);
+  digitalWrite(LED2, HIGH);
+
+  //Sensor 発光off
+  pinMode(SLED_F, OUTPUT);
+  pinMode(SLED_S, OUTPUT);
+  digitalWrite(SLED_F, LOW);
+  digitalWrite(SLED_S, LOW);
+
+  delay(2000);
+
+  g_timer1 = timerBegin(1, 80, true);  //1us
+  timerAttachInterrupt(g_timer1, &onTimer1, true);
+  timerAlarmWrite(g_timer1, 250, true);  //4kHz
+  timerAlarmEnable(g_timer1);
+
+  g_allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&g_support, 0, NULL, &g_allocator));
+
+  // create g_node
+  RCCHECK(rclc_node_init_default(&g_node, "micro_ros_pico_node", "", &g_support));
+
+  // create g_publisher
+  RCCHECK(rclc_publisher_init_default(
+    &g_publisher_sensor, &g_node, ROSIDL_GET_MSG_TYPE_SUPPORT(pico_msgs, msg, LightSensor),
+    "pico_sensor"));
+
+  RCCHECK(rclc_publisher_init_default(
+    &g_publisher_battery, &g_node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16), "pico_battery"));
+
+  xTaskCreateUniversal(
+    //  xTaskCreatePinnedToCore(
+    publisherTask, "publisherTask", 4096, NULL, 1, NULL,
+    //    PRO_CPU_NUM
+    //    APP_CPU_NUM
+    CONFIG_ARDUINO_RUNNING_CORE);
+}
+
+void loop() { delay(10); }
